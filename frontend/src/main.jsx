7f2407ded94a39f3f912c5c9105dbc05fecee283
Ashwin-J01 <ashwinj.23it@kongu.edu>
2026-03-16 01:56:54 +0530

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+
+// Configure axios with API base URL
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
